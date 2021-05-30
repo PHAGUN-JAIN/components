4717c90e0b1f4343205c4347e8f78cec222a287b
PHAGUN JAIN <PHAGUNJAIN@protonmail.com>
2021-05-30 12:59:13 +0530

    initial commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -12,6 +12,14 @@ const img_style = {
   width: "100%",
   borderRadius: "15px 15px 0 0"
 };
+const btn_style = {
+  lineHeight: "1.4rem",
+  border: "1px solid black",
+  borderRadius: "12px",
+  backgroundColor: "grey",
+  color: "#fff",
+  fontWeight: "800"
+};
 export default function Cards(props) {
   return (
     <>
@@ -19,7 +27,9 @@ export default function Cards(props) {
         <img style={img_style} src={link} alt="randon" />
         <h4>{props.title}</h4>
         <p>{props.desc}</p>
-        <button href={props.link}>Watch Here!</button>
+        <button style={btn_style} href={props.link}>
+          Watch Here!
+        </button>
       </div>
     </>
   );
